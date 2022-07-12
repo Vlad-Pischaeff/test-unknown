1f01efc5a4cf6e1329916f4e21f3f2025ded51e5
Vlad-Pischaeff <pischaeff@gmail.com>
2022-07-12 10:46:17 +0500

    update

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,11 @@ const { randomString } = require('make-random');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+const checkBase64 = string => {
+    const B64_REGEX = /^data:.*;base64,([0-9a-zA-Z+\\/]{4})*(([0-9a-zA-Z+\\/]{2}==)|([0-9a-zA-Z+\\/]{3}=))?$/i ;
+    return B64_REGEX.test(string);
+}
+
 module.exports = app => {
     app.use('/upload', express.static('./upload'));
     app.use('/api/users', require('../routes/Users'));
@@ -15,11 +20,12 @@ module.exports = app => {
             res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
         });
     }
-
+    /**
+     * получаем base64 кодированный файл
+     * генерим имя и сохраняем в /upload
+     * возвращаем имя файла
+     */
     app.post('/api/upload', async (req, res, next) => {
-        // получаем base64 кодированный файл
-        // генерим имя и сохраняем в /upload
-        // возвращаем имя файла
         const { photo } = req.body;
         const name = await randomString(15);
 
@@ -30,9 +36,4 @@ module.exports = app => {
 
         res.send({ name });
     })
-
-    function checkBase64(string){
-        const B64_REGEX = /^data:.*;base64,([0-9a-zA-Z+\\/]{4})*(([0-9a-zA-Z+\\/]{2}==)|([0-9a-zA-Z+\\/]{3}=))?$/i ;
-        return B64_REGEX.test(string);
-    }
 };
